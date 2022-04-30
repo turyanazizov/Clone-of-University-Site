7816e6fe2d36cde8a4112362c00d1563281e24cd
Turyan Azizov <turyanazizov13@gmail.com>
2022-04-30 13:54:19 +0400

    commitc

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls', namespace='home')),
+    path('/', include('home.urls', namespace='home')),
     path('personal/', include('personal.urls', namespace='personal')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
