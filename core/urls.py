2c91e9b14250c3f37af3d62216a38fffb92a8195
Turyan Azizov <turyanazizov13@gmail.com>
2022-04-30 15:19:21 +0400

    hsd

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
     path('personal/', include('personal.urls', namespace='personal')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
