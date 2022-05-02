b079a3300e51217a760d6cdd1394e043ec05a322
Turyan Azizov <turyanazizov13@gmail.com>
2022-05-02 12:58:00 +0400

    comit

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-
+# main url https://debis-deu-edu-tr.herokuapp.com/Ogrenci_Giris_Sayfasi/debis.php/debis.deu.edu.tr
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls', namespace='home')),
