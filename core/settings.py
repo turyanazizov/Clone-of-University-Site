51d00e51ec05c12ddb8dc40d0c12ac00b9302306
Turyan Azizov <turyanazizov13@gmail.com>
2022-05-02 13:56:47 +0400

    comit

--- core/settings.py
+++ core/settings.py
@@ -69,10 +69,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': 'demo_test',
         'USER' : 'postgres',
         'PASSWORD' : '1234',
-        'HOST' : '127.0.0.1',
+        'HOST' : 'localhost',
         'PORT' : '5432',
     }
 }
